044ecfec1ab2e4ee5362f3a7a790838ce18bd09c
javedwasim <javedafaq@gmail.com>
2019-05-02 15:06:04 +0500

    add category_id in post table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserTableSeeder::class);
-         $this->call(PostTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(PostTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
     }
 }
