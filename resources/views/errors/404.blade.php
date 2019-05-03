e2e08c5eda17bcbf7cfa4d63c0d3741fb4e28c23
javedwasim <javedafaq@gmail.com>
2019-05-03 12:34:40 +0500

    style 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,12 @@
-@extends('errors::minimal')
+@extends('layouts.main')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 page-not-found">
+                <h2>Page Not Found</h2>
+                <p>Sorry, the page you're looking for couldn't be found</p>
+            </div>
+        </div>
+    </div>
+@endsection
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
